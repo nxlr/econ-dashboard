1a4e83a578b77beb9189276f42fa83b53184ca60
Sachin Sharma <amrahs.nihcas@gmail.com>
2022-01-07 23:46:12 +0530

    add waiter package to init.R

--- init.R
+++ init.R
@@ -1,6 +1,7 @@
 packages <- c("shiny", "dplyr", "plotly", "ggthemes", "thematic", "showtext",
               "readxl", "tidyr", "ggstream", "viridis", "hrbrthemes", "reshape2", 
-              "highcharter", "shinydashboard", "shinydashboardPlus", "shinyWidgets")
+              "highcharter", "shinydashboard", "shinydashboardPlus", "shinyWidgets",
+              "waiter")
 
 install_if_missing <- function(p) {
   if (!p %in% rownames(installed.packages())) {
